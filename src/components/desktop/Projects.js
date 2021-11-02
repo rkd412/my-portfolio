47435ac12b89c91751fe27d48f45c434d64224f8
Robert Davis <rkdavis15@gmail.com>
2021-11-02 15:08:54 -0400

    Changed Fade animations

--- src/components/desktop/Projects.js
+++ src/components/desktop/Projects.js
@@ -25,102 +25,122 @@ const Projects = () => {
         theme.isNight ? styles["night-project-box"] : styles["day-project-box"]
       }
     >
-      <div
-        className={
-          theme.isNight ? styles["night-img-div"] : styles["day-img-div"]
-        }
-      >
-        <h1>Outdoor E-Commerce Page</h1>
-        <img
-          className={styles["image"]}
-          loading="lazy"
-          src={ECommerce}
-          alt="E-Commerce Site"
-          onClick={clickHandler}
-          id="https://wonderful-ptolemy-65f598.netlify.app/"
-        />{" "}
-        <a href="https://github.com/rkd412/outdoor-ecommerce" target="_blank" rel="noreferrer">
-          Source Code
-        </a>
-      </div>
-      <div
-        className={
-          theme.isNight ? styles["night-text-div"] : styles["day-text-div"]
-        }
-      >
-        <ul>
-          <li>CSS</li>
-          <li>HTML</li>
-          <li>Javascript</li>
-          <li>React</li>
-          <li>Material UI</li>
-        </ul>
-      </div>
+      <Fade left>
+        <div
+          className={
+            theme.isNight ? styles["night-img-div"] : styles["day-img-div"]
+          }
+        >
+          <h1>Outdoor E-Commerce Page</h1>
+          <img
+            className={styles["image"]}
+            loading="lazy"
+            src={ECommerce}
+            alt="E-Commerce Site"
+            onClick={clickHandler}
+            id="https://wonderful-ptolemy-65f598.netlify.app/"
+          />{" "}
+          <a
+            href="https://github.com/rkd412/outdoor-ecommerce"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Source Code
+          </a>
+        </div>
+     
+        <div
+          className={
+            theme.isNight ? styles["night-text-div"] : styles["day-text-div"]
+          }
+        >
+          <ul>
+            <li>CSS</li>
+            <li>HTML</li>
+            <li>Javascript</li>
+            <li>React</li>
+            <li>Material UI</li>
+          </ul>
+        </div>
+      </Fade>
 
-      <div
-        className={
-          theme.isNight ? styles["night-text-div"] : styles["day-text-div"]
-        }
-      >
-        <ul>
-          <li>CSS</li>
-          <li>HTML</li>
-          <li>Javascript</li>
-          <li>React</li>
-          <li>Material UI</li>
-        </ul>
-      </div>
-      <div
-        className={
-          theme.isNight ? styles["night-img-div"] : styles["day-img-div"]
-        }
-      >
-        <h1>Security API App</h1>
-        <img
-          className={styles["image"]}
-          loading="lazy"
-          src={SecurityApp}
-          alt="Security API App"
-          onClick={clickHandler}
-          id="https://infallible-khorana-72773f.netlify.app/"
-        />
+      <Fade right>
+        <div
+          className={
+            theme.isNight ? styles["night-text-div"] : styles["day-text-div"]
+          }
+        >
+          <ul>
+            <li>CSS</li>
+            <li>HTML</li>
+            <li>Javascript</li>
+            <li>React</li>
+            <li>Material UI</li>
+          </ul>
+        </div>
+   
+        <div
+          className={
+            theme.isNight ? styles["night-img-div"] : styles["day-img-div"]
+          }
+        >
+          <h1>Security API App</h1>
+          <img
+            className={styles["image"]}
+            loading="lazy"
+            src={SecurityApp}
+            alt="Security API App"
+            onClick={clickHandler}
+            id="https://infallible-khorana-72773f.netlify.app/"
+          />
+          <a
+            href="https://github.com/rkd412/security-api-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Source Code
+          </a>
+        </div>
+      </Fade>
 
-        <a href="https://github.com/rkd412/security-api-app" target="_blank" rel="noreferrer">
-          Source Code
-        </a>
-      </div>
-
-      <div
-        className={
-          theme.isNight ? styles["night-img-div"] : styles["day-img-div"]
-        }
-      >
-        <h1>Monty Hall Game App</h1>
-        <img
-          className={styles["image"]}
-          loading="lazy"
-          src={MontyHall}
-          alt="Monty Hall Game"
-          onClick={clickHandler}
-          id="https://eloquent-clarke-721698.netlify.app/"
-        />
-
-        <a href="https://github.com/rkd412/monty-hall" target="_blank" rel="noreferrer">
-          Source Code
-        </a>
-      </div>
-      <div
-        className={
-          theme.isNight ? styles["night-text-div"] : styles["day-text-div"]
-        }
-      >
-        <ul>
-          <li>CSS</li>
-          <li>HTML</li>
-          <li>Javascript</li>
-          <li>React</li>
-        </ul>
-      </div>
+      <Fade left>
+        <div
+          className={
+            theme.isNight ? styles["night-img-div"] : styles["day-img-div"]
+          }
+        >
+          <h1>Monty Hall Game App</h1>
+          <img
+            className={styles["image"]}
+            loading="lazy"
+            src={MontyHall}
+            alt="Monty Hall Game"
+            onClick={clickHandler}
+            id="https://eloquent-clarke-721698.netlify.app/"
+          />
+          <a
+            href="https://github.com/rkd412/monty-hall"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Source Code
+          </a>
+        </div>
+    
+        <div
+          className={
+            theme.isNight ? styles["night-text-div"] : styles["day-text-div"]
+          }
+        >
+          <ul>
+            <li>CSS</li>
+            <li>HTML</li>
+            <li>Javascript</li>
+            <li>React</li>
+          </ul>
+        </div>
+      </Fade>
+      
     </div>
   );
 };
