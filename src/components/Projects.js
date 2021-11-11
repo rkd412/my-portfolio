ced38e29558df1771edb70502a83ee8a19c69fa8
Robert Davis <rkdavis412@gmail.com>
2021-11-11 16:34:34 -0500

    changed photo previews

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -60,7 +60,7 @@ const Projects = () => {
             }
           >
             <ul>
-              <li>Built with HTML, CSS, Javascript, React, Material UI.</li>
+              <li>Built with HTML, CSS, Javascript, React, MaterialUI</li>
               <li>
                 Security API app that includes 3 different functioning APIs.
               </li>
@@ -88,6 +88,7 @@ const Projects = () => {
                 >
                   Live Demo
                 </a>
+                .
               </li>
             </ul>
           </div>
@@ -103,7 +104,7 @@ const Projects = () => {
             }
           >
             <ul>
-              <li>Built with HTML, CSS, Javascript, React, Material UI.</li>
+              <li>Built with HTML, CSS, Javascript, React, MaterialUI.</li>
 
               <li>
                 Outdoor E-Commerce site that includes a functioning cart and
@@ -112,8 +113,7 @@ const Projects = () => {
 
               <li>
                 First attempt using a design library. The premade components
-                were very helpful but it was challenging learning a new styling
-                system.
+                were very helpful but it was challenging learning a new system.
               </li>
 
               <li>
@@ -197,16 +197,11 @@ const Projects = () => {
             }
           >
             <ul>
-              <li>Built with CSS, HTML, Javascript, and React.</li>
+              <li>Built with CSS, HTML, Javascript, and React</li>
               <li>A game illustrating the Monty Hall problem.</li>{" "}
               <li>
                 Thinking through the problem and getting the logic behind the
                 game correct was challenging but fun.
-              </li>
-              <li>
-                I really wanted to maintain accuracy of the original brain
-                teaser in code the best I could and I think I did a pretty good
-                job in that regard.
               </li>
               <li>
                 <a
@@ -224,7 +219,7 @@ const Projects = () => {
                   target="_blank"
                   rel="noreferrer"
                 >
-                  Live Demo.
+                  Live Demo
                 </a>
                 .
               </li>
@@ -239,7 +234,7 @@ const Projects = () => {
             theme.isNight ? styles["night-outro"] : styles["day-outro"]
           }
         >
-          <h2>Hey, thanks for checking out my projects.</h2>
+          <h2>Hey, thanks for checking out my projects page.</h2>
           <h4>
             {" "}
             I would suggest heading to my{" "}
