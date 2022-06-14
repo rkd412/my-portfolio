e1b211449086a49bbc4d99abd5372cfbef93a3a5
Robert Davis <rkdavis412@gmail.com>
2022-06-14 11:34:09 -0400

    Fixed??????

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -246,7 +246,7 @@ const Projects = () => {
             >
               Online Stores
             </a>{" "}
-            to see what I've been working on - there are several e-commerce sites
+            to see what I've been working on - npm there are several e-commerce sites
             to check out. My{" "}
             <a
               aria-label="CodePen Page"
